a86312c493d96015d66f2d60cc55b2836a6cd206
Plenoh <tylrd629@gmail.com>
2022-03-22 11:03:39 +0900

    Login/Logout feature

--- nav.php
+++ nav.php
@@ -6,13 +6,13 @@
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav ml-auto">
 <?php
-//if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true)
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true)
 {
 ?>
                 <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
 <?php
 }
-//else
+else
 {
 ?>
                 <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
