042944bdc650a6e658e4ce0ad73683ca985e837f
Plenoh <tylrd629@gmail.com>
2022-04-07 17:42:11 +0900

    Replace the logo

--- nav.inc.php
+++ nav.inc.php
@@ -1,5 +1,5 @@
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <a class="navbar-brand" href="index.php">Scibx Bookmarks</a>
+        <a class="navbar-brand" href="index.php"><img src="images/bookmark-256.png" alt="Bookmarks" class="logo"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
