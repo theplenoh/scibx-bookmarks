38a8d9d362c64860805ec3292679497cef38f0b5
theplenoh <tylrd529@gmail.com>
2022-04-07 19:24:33 +0900

    Add search input field

--- nav.inc.php
+++ nav.inc.php
@@ -1,10 +1,13 @@
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <a class="navbar-brand" href="index.php"><img src="images/bookmark-256.png" alt="Bookmarks" class="logo"></a>
+        <a class="navbar-brand mr-auto" href="index.php"><img src="images/bookmark-256.png" alt="Bookmarks" class="logo"></a>
+        <form class="mx-2" action="">
+            <input class="form-control" type="text" placeholder="Search">
+        </form>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav ml-auto">
+        <div class="collapse navbar-collapse flex-grow-0" id="collapsibleNavbar">
+            <ul class="navbar-nav">
 <?php
 if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true)
 {
