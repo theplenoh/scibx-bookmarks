e1301f93bdf95f849d33f944d4490006267fa8bb
Plenoh <tylrd629@gmail.com>
2022-07-12 10:43:49 +0900

    Feature: instantly add a bookmark

---
+++ insert_instant.php
@@ -0,0 +1,32 @@
+<?php
+require_once "common.php";
+
+session_start();
+
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true)
+{
+    echo<<<EOT
+<script>
+alert("로그인이 안된 상태입니다.\\n로그인 페이지로 이동합니다.");
+location.href="login.php";
+</script>
+EOT;
+exit;
+}
+
+$flag_loggedin = false;
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true)
+    $flag_loggedin = true;
+
+$URL = $_POST['URL'];
+$time = date("Y-m-d H:i", time());
+$publicity = "private";
+$title = page_title($URL);
+
+$query = "INSERT INTO bookmarks_entries (URL, title, time, publicity) VALUES('{$URL}', '{$title}', '{$time}', '{$publicity}')";
+
+mysqli_query($conn, $query);
+mysqli_close($conn);
+
+header("Location: list.php");
+?>
