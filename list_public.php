48aacdc78af4f1b6b01c4fc313d8afcd9b6e549c
Plenoh <tylrd629@gmail.com>
2022-07-21 21:21:32 +0900

    Prepare list_public.php to display entries in public

--- list_public.php
+++ list_public.php
@@ -71,6 +71,7 @@ else
 <?php
     while($entry = mysqli_fetch_array($result))
     {
+        $entryID = $entry['entryID'];
         $title = $entry['title'];
         $URL = $entry['URL'];
         $datetime = $entry['time'];
@@ -94,9 +95,13 @@ else
 ?>
                     </p>
                     <div class="btn-group btn-group-sm">
+<?php
+/*
                         <a class="btn btn-sm m-0 p-1 px-1">Edit</a>
                         <a class="btn btn-sm m-0 p-1 px-1">Delete</a>
-                        <a class="btn btn-sm m-0 p-1 px-1">Make Public</a>
+*/
+?>
+<?php if($flag_loggedin) { echo "<a class=\"btn btn-sm m-0 p-1 px-1\" href=\"make_private.php?entryID=$entryID\">Make Private</a>"; } else { echo ""; } ?>
                     </div>
                 </div>
                 <div class="card-footer p-1 px-2 small"><?php echo $datetime; ?></div>
