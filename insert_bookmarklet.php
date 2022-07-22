a3f66535f7da84f7b6cf0194e43303e15ea1865a
Plenoh <tylrd629@gmail.com>
2022-07-22 14:36:00 +0900

    Feature: Add by JS bookmarklet

---
+++ insert_bookmarklet.php
@@ -0,0 +1,33 @@
+<?php
+require_once "common.php";
+
+session_start();
+
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true)
+{
+    echo<<<EOT
+<script>
+alert("로그인이 안된 상태입니다.\\n로그인 페이지로 이동합니다.");
+location.href="login.php";
+</script>
+EOT;
+exit;
+}
+
+$flag_loggedin = false;
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true)
+    $flag_loggedin = true;
+
+$URL = $_POST['URL'];
+$title = $_POST['title'];
+$note = $_POST['note'];
+$time = date("Y-m-d H:i", time());
+$publicity = "private";
+
+$query = "INSERT INTO bookmarks_entries (URL, title, note, time, publicity) VALUES('{$URL}', '{$title}', '{$note}', '{$time}', '{$publicity}')";
+
+mysqli_query($conn, $query);
+mysqli_close($conn);
+
+header("Location: list.php");
+?>
