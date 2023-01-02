d9d33d3d6c95c3c82fc53795b6aafd0cc4aa2be3
Plenoh <tylrd629@gmail.com>
2023-01-02 15:26:18 +0900

    Replace page_title func

--- common.php
+++ common.php
@@ -36,19 +36,20 @@ function get_day($date, $locale)
     }
 }
 
-function page_title($url)
+function page_title($urlpage)
 {
-    $fp = file_get_contents($url);
-    if(!$fp)
-        return null;
+    $title = '';
+    $dom = new DOMDocument();
 
-    $res = preg_match("/<title>(.*)<\/title>/siU", $fp, $title_matches);
-    if(!$res)
-        return null;
+    if(@$dom->loadHTMLFile($urlpage)) {
+        $list = $dom->getElementsByTagName("title");
+        if ($list->length > 0) {
+            $title = $list->item(0)->textContent;
+        }
+    }
 
-    // Clean up title: remove EOL's and excessive whitespace.
-    $title = preg_replace('/\s+/', ' ', $title_matches[1]);
     $title = trim($title);
+    $title = sanitize($title);
     return $title;
 }
 
