fd5997634f8f3310af326087c7670c76ac076bc1
Plenoh <tylrd529@gmail.com>
2024-04-11 16:26:29 +0900

    Create a branch depending on login status

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-header("Location: list_public.php");
+require_once "common.php";
+
+session_start();
+
+$flag_loggedin = false;
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true)
+    $flag_loggedin = true;
+
+if(!$flag_loggedin)
+    header("Location: list_public.php");
+else
+    header("Location: list.php");
+
 exit;
 ?>
