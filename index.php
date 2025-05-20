26bdc7bc33ea4ac8435e8112baa84d653d1a8362
Plenoh <tylrd529@gmail.com>
2025-05-20 14:09:01 +0900

    Edit redirection control in index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ $flag_loggedin = false;
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true)
     $flag_loggedin = true;
 
-if(!$flag_loggedin)
-    header("Location: list_public.php");
-else
+if($flag_loggedin === true)
     header("Location: list.php");
+else
+    header("Location: list_public.php");
 
 exit;
 ?>
